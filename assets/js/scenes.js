dd261ff1dc884f26b401feddcc99c17784a133da
CruzZFire <a.cruzana88@gmail.com>
2020-06-03 22:54:14 +0200

    Added more Scenes to the API. Changed text colour for more contrast. Adjusted bottom margin for mobile devices.

--- assets/js/scenes.js
+++ assets/js/scenes.js
@@ -31,7 +31,7 @@ const scenes = [
   },
   {
     id: 1, //Scene One - no choice
-    text: "First Scene - Found map choice",
+    text: "Yo Ho Ho! You have found a bottle with a map inside at the beach!",
     background: "/assets/images/scenes/1.png",
     dialog: "",
     options: [
@@ -40,7 +40,7 @@ const scenes = [
         nextScene: 1.1
       },
       {
-        text: "Drop Map",
+        text: "Drop Botlle",
         nextScene: 1.2
       },
       {
@@ -55,7 +55,7 @@ const scenes = [
   },
   {
     id: 1.1, //Scene One - choice
-    text: "Read map - Continue",
+    text: "The map shows an island you seem to recognize. Will the red 'X' mean there's a hidden treasure?",
     background: "/assets/images/scenes/1.1.png",
     dialog: "",
     options: [
@@ -81,7 +81,7 @@ const scenes = [
     id: 1.2, //Scene One - no choice
     text: "",
     background: "/assets/images/scenes/66.png",
-    dialog: "Drop map - no can do",
+    dialog: "Ahoy! I think curiosity is too powerfull!!",
     options: [
       {
         text: "Read Map",
@@ -105,18 +105,18 @@ const scenes = [
     id: 2, //Scene Two - first choice
     text: "",
     background: "/assets/images/scenes/66.png",
-    dialog: "Second Scene - First choice",
+    dialog: "Aaarr, but I'm no captain. How should I be on route?",
     options: [
       {
-        text: "Enlist",
+        text: "Enlist a Pirate Crew",
         nextScene: 2.1
       },
       {
-        text: "Hide",
+        text: "Hide on a Ship's Hold",
         nextScene: 2.2
       },
       {
-        text: "Steal",
+        text: "Rob a Ship",
         nextScene: 2.3
       },
       {
@@ -126,14 +126,14 @@ const scenes = [
     ]
   },
   {
-    id: 2.1, //Scene Two - first choice
-    text: "Enlist - Continue",
+    id: 2.1, //Scene Two-One - Continue
+    text: "You made it on board, 'no prey, no pay' they said...",
     background: "/assets/images/scenes/2.1.png",
     dialog: "",
     options: [
       {
         text: "Continue",
-        nextScene: 2
+        nextScene: 2.11
       },
       {
         text: "-",
@@ -150,14 +150,38 @@ const scenes = [
     ]
   },
   {
-    id: 2.2, //Scene Two - first choice
-    text: "Hide Scene - Continue",
+    id: 2.11, //Scene Two-Eleven - second choice
+    text: "",
+    background: "/assets/images/scenes/66.png",
+    dialog: "Avast ye! We going the wrong direction!!",
+    options: [
+      {
+        text: "Trick the Ship's Helm",
+        nextScene: 2.111
+      },
+      {
+        text: "Try Talking to the Captain",
+        nextScene: 2.112
+      },
+      {
+        text: "Escape in the Rowboat",
+        nextScene: 2.113
+      },
+      {
+        text: "-",
+        nextScene: 2.11
+      }
+    ]
+  },
+  {
+    id: 2.2, //Scene Two-Two - Continue
+    text: "Aye, you are hidden on the cellar. Time for a sip of rum and take a 'caulk'...",
     background: "/assets/images/scenes/2.2.png",
     dialog: "",
     options: [
       {
         text: "Continue",
-        nextScene: 2
+        nextScene: 2.21
       },
       {
         text: "-",
@@ -174,14 +198,38 @@ const scenes = [
     ]
   },
   {
-    id: 2.3, //Scene Two - first choice
-    text: "Steal Scene - Continue",
+    id: 2.21, //Scene Two-TwentyOne - second choice
+    text: "",
+    background: "/assets/images/scenes/66.png",
+    dialog: "Blimey!!!! 'Me' got caught!! Need to do something, I don't want to feed the fish just yet",
+    options: [
+      {
+        text: "Challenge the Captain",
+        nextScene: 2.211
+      },
+      {
+        text: "Enroll the Crew",
+        nextScene: 2.212
+      },
+      {
+        text: "Jump Over the Edge",
+        nextScene: 2.213
+      },
+      {
+        text: "-",
+        nextScene: 2.21
+      }
+    ]
+  },
+  {
+    id: 2.3, //Scene Two-Three - Continue
+    text: "Yo Ho Ho!! This fools went to the tavern all at time. Aye, time to raise the anchor!",
     background: "/assets/images/scenes/2.3.png",
     dialog: "",
     options: [
       {
         text: "Continue",
-        nextScene: 2
+        nextScene: 2.31
       },
       {
         text: "-",
@@ -196,5 +244,29 @@ const scenes = [
         nextScene: 2.3
       }
     ]
-  }
+  },
+  {
+    id: 2.31, //Scene Two-ThirtyOne - second choice
+    text: "",
+    background: "/assets/images/scenes/66.png",
+    dialog: "'Shiver Me Timbers'! I see through my spyglass that they are chasing me!!!",
+    options: [
+      {
+        text: "Turn West",
+        nextScene: 2.311
+      },
+      {
+        text: "Turn East",
+        nextScene: 2.312
+      },
+      {
+        text: "Maintain Route",
+        nextScene: 2.313
+      },
+      {
+        text: "Go For Them!",
+        nextScene: 2.31
+      }
+    ]
+  },
 ];
