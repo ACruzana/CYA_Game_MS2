0954d59db6f0b1120b1bb6f4ff7cb8fa0f1ea773
CruzZFire <a.cruzana88@gmail.com>
2020-06-11 11:04:02 +0200

    Added mute effect option to the battle sound effects at features.js

--- assets/js/features.js
+++ assets/js/features.js
@@ -3,19 +3,25 @@
 const buttonSound = document.getElementById("button-sound");
 const ambientMusic = document.getElementById("ambient-music");
 
-function soundOn () {
+function soundOn() {
+  swordHitSound.muted = false;
+  gunSound.muted = false;
+  swordParrySound.muted = false;
   buttonSound.muted = false;
   $("#soundOn").addClass("underlined");
   $("#soundOff").removeClass("underlined");
 }
 
-function soundOff () {
+function soundOff() {
+  swordHitSound.muted = true;
+  gunSound.muted = true;
+  swordParrySound.muted = true;
   buttonSound.muted = true;
   $("#soundOff").addClass("underlined");
   $("#soundOn").removeClass("underlined");
 }
 
-function musicOn () {
+function musicOn() {
   ambientMusic.play();
   ambientMusic.volume = 0.2;
   ambientMusic.loop = true;
@@ -23,19 +29,19 @@ function musicOn () {
   $("#musicOff").removeClass("underlined");
 }
 
-function musicOff () {
+function musicOff() {
   ambientMusic.pause();
   $("#musicOff").addClass("underlined");
   $("#musicOn").removeClass("underlined");
 }
 
-function lightOn () {
+function lightOn() {
   $("body").css('background', 'url("assets/images/site-bg.jpg") repeat top');
   $("#lightOn").addClass("underlined");
   $("#darkOn").removeClass("underlined");
 }
 
-function darkOn () {
+function darkOn() {
   $("body").css('background', 'url("assets/images/site-bg-dark.jpg") repeat top');
   $("#darkOn").addClass("underlined");
   $("#lightOn").removeClass("underlined");
