bb2a8a8671b76c3a575ef38b59da5edaf0b6d87e
CruzZFire <a.cruzana88@gmail.com>
2020-06-11 12:35:49 +0200

    Added texts to battle interaction outputs for player to follow at xbattle.js

--- assets/js/xbattle.js
+++ assets/js/xbattle.js
@@ -8,81 +8,78 @@ const battleText = document.getElementById("battleText"); //Text imputs at battl
 /* Battle Option Execution */
 
 function actionResult(action) { //Output of each battle action
-  const opponentMoves = ["swordAttack", "pistolAttack", "swordDefend", "pistolDefend"]; //Opponent random move
+  const opponentMoves = ["swordAttack", "pistolAttack", "swordDefend", "pistolDefend"]; //Opponent random move pool
   let opponentRng = Math.floor(Math.random() * 4);
   let counterAction = opponentMoves[opponentRng];
 
   if (action === "swordAttack") { //Sword Att vs Opp
     if (counterAction === "swordAttack") {
       swordHitSound.play();
-      console.log("you attacked with the sword and boss also used the sword");
+      battleText.innerText = "You both swing your blades, you both get hit, but you are both weaker now...";
     } else if (counterAction === "pistolAttack") {
       swordHitSound.play();
       gunSound.play();    
-      console.log("you attacked with the sword and boss used the pistol");
+      battleText.innerText = "You lunge forward with your sword and hit him but you also receive a gun shot!!";
     } else if (counterAction === "swordDefend") {
       swordParrySound.play();
-      console.log("you attacked with the sword but boss parried");
+      battleText.innerText = "You swing your sword, but he masterfully parries the attack rejecting it";
     } else {
       swordHitSound.play();
-      console.log("you attacked with the sword, boss tried to evade but failed");
+      battleText.innerText = "You lunge forward with your sword and hit him as he tried to strafe and evade";
     }
   } else if (action === "pistolAttack") { //Pistol Att vs Opp
     if (counterAction === "swordAttack") {
       swordHitSound.play();
       gunSound.play();    
-      console.log("you shot your flintlock but boss used the sword");
+      battleText.innerText = "You make use of your flintlock as he lunges forward to reach you with his sword";
     } else if (counterAction === "pistolAttack") {
       gunSound.play();    
       gunSound.play();    
-      console.log("you shot your flintlock and boss also used the pistol");
+      battleText.innerText = "You both shot your guns, you both get hit, but you are both weaker now...";
     } else if (counterAction === "swordDefend") {
       gunSound.play();    
-      console.log("you shot your flintlock, boss tried to parry but failed");
+      battleText.innerText = "He really tried to parry a gun bullet?!? Definitively that did not work";
     } else {
       gunSound.play();    
-      console.log("you shot your flintlock boss evaded");
+      battleText.innerText = "You shot your flintlock, but he strafed swiftly and evaded the shot";
     }
   } else if (action === "swordDefend") { //Sword Def vs Opp
     if (counterAction === "swordAttack") {
       swordParrySound.play();
-      console.log("you parried the boss attack with the sword");
+      battleText.innerText = "He swings his sword, but you masterfully parry the attack rejecting it";
     } else if (counterAction === "pistolAttack") {
       gunSound.play();    
-      console.log("you tried parrying but boss also used the pistol");
+      battleText.innerText = "You really tried to parry a gun bullet?!? Definitively that did not work";
     } else if (counterAction === "swordDefend") {
-      console.log("you both look like fools parrying for nothing");
+      battleText.innerText = "You are both keeping a defensive stance with the sword, the crowd gets nervous...";
     } else {
-      console.log("your are parrying and boss is evading");
+      battleText.innerText = "You keep your sword up, he keeps strafe jumping, a duel can't get more ridiculous...";
     }
   } else {
     if (counterAction === "swordAttack") { //Pistol Def vs Opp
       swordHitSound.play();
-      console.log("you tried evading but the boss attacked with the sword");
+      battleText.innerText = "You try to strafe and evade, but he lunges forward with his sword and hits you";
     } else if (counterAction === "pistolAttack") {
       gunSound.play();    
-      console.log("you evaded that shot, boss used the pistol");
+      battleText.innerText = "He shot at you with his flintlock, but you strafed swiftly and evaded the shot";
     } else if (counterAction === "swordDefend") {
-      console.log("you are evading, boss is parrying");
+      battleText.innerText = "You are strafe jumping, he keeps his sword up, a duel can't get more ridiculous...";
     } else {
-      console.log("your are both strafing like mad men");
+      battleText.innerText = "You are both strafing to the side in circles... Funny, but this could never end...";
     }
   }
 }
 
-/* Battle Option Execution */
+/* Battle Option Processing */
 
 function battleOption(option) {
-  console.log("battleOption on going");
   const actionTaken = option.action;
-  console.log(actionTaken);
   return actionResult(actionTaken);
 }
 
 /* Main Battle Function */
 
 function battle(sceneNumber) {
-  console.log("battle on going");
   const scene = scenes.find(scene => scene.id === sceneNumber); //Access the object at scenes.js
 
   bubbleText.innerText = ""; //Clear previous texts
@@ -107,8 +104,6 @@ function battle(sceneNumber) {
   $(screenElem).css({
     "background": "red"
   }); // toDo visually
-
-  battleText.innerText = scene.text; //Screen text info type toDo
 
   /* win showScene(78);*/ //future returns
   /* lose showScene(79);*/
