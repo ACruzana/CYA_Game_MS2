94bd5cac670ea8d1609f38848c18f667170e19c7
CruzZFire <a.cruzana88@gmail.com>
2020-06-11 10:53:47 +0200

    Added sound effects to the interaction output logic at xbattle.js

--- assets/js/xbattle.js
+++ assets/js/xbattle.js
@@ -1,9 +1,9 @@
 /* Battle Scene Logic File */
 
-const swordHitSound = document.getElementById("sword-hit-sound"); //Sound Effects
+const swordHitSound = document.getElementById("sword-hit-sound"); //Sound Effects for battle
 const swordParrySound = document.getElementById("sword-parry-sound");
 const gunSound = document.getElementById("gun-sound");
-const battleText = document.getElementById("battleText"); //Text imputs
+const battleText = document.getElementById("battleText"); //Text imputs at battle
 
 /* Battle Option Execution */
 
@@ -14,28 +14,41 @@ function actionResult(action) { //Output of each battle action
 
   if (action === "swordAttack") { //Sword Att vs Opp
     if (counterAction === "swordAttack") {
+      swordHitSound.play();
       console.log("you attacked with the sword and boss also used the sword");
     } else if (counterAction === "pistolAttack") {
+      swordHitSound.play();
+      gunSound.play();    
       console.log("you attacked with the sword and boss used the pistol");
     } else if (counterAction === "swordDefend") {
+      swordParrySound.play();
       console.log("you attacked with the sword but boss parried");
     } else {
+      swordHitSound.play();
       console.log("you attacked with the sword, boss tried to evade but failed");
     }
   } else if (action === "pistolAttack") { //Pistol Att vs Opp
     if (counterAction === "swordAttack") {
+      swordHitSound.play();
+      gunSound.play();    
       console.log("you shot your flintlock but boss used the sword");
     } else if (counterAction === "pistolAttack") {
+      gunSound.play();    
+      gunSound.play();    
       console.log("you shot your flintlock and boss also used the pistol");
     } else if (counterAction === "swordDefend") {
+      gunSound.play();    
       console.log("you shot your flintlock, boss tried to parry but failed");
     } else {
+      gunSound.play();    
       console.log("you shot your flintlock boss evaded");
     }
   } else if (action === "swordDefend") { //Sword Def vs Opp
     if (counterAction === "swordAttack") {
+      swordParrySound.play();
       console.log("you parried the boss attack with the sword");
     } else if (counterAction === "pistolAttack") {
+      gunSound.play();    
       console.log("you tried parrying but boss also used the pistol");
     } else if (counterAction === "swordDefend") {
       console.log("you both look like fools parrying for nothing");
@@ -44,8 +57,10 @@ function actionResult(action) { //Output of each battle action
     }
   } else {
     if (counterAction === "swordAttack") { //Pistol Def vs Opp
+      swordHitSound.play();
       console.log("you tried evading but the boss attacked with the sword");
     } else if (counterAction === "pistolAttack") {
+      gunSound.play();    
       console.log("you evaded that shot, boss used the pistol");
     } else if (counterAction === "swordDefend") {
       console.log("you are evading, boss is parrying");
